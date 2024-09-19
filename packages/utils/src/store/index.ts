79b89fa11b837dde0bfe558d72d364a59997ca76
MuRong <hi@mrong.me>
2024-09-19 18:28:45 +0800

    feat(utils): get memory store keys by pattern

--- packages/utils/src/store/index.ts
+++ packages/utils/src/store/index.ts
@@ -1,3 +1,5 @@
-export { Store, StoreConfig, Stores, memoryStore, MemoryStore, MemoryCache, MemoryConfig } from 'cache-manager'
+export { Store, StoreConfig, Stores } from 'cache-manager'
 export { redisStore } from './redis'
 export * from './manager'
+export { memoryStore } from './memory'
+export type { MemoryStore, MemoryCache, MemoryConfig } from './memory'
