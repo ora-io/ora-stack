33e93039f34e08996c52c900d2fad5e3fac57f8a
MuRong <hi@mrong.me>
2024-09-24 23:04:57 +0800

    feat(orap): export default Orap

--- packages/orap/src/index.ts
+++ packages/orap/src/index.ts
@@ -1,6 +1,10 @@
+import { Orap } from './orap'
+
 export * from './orap'
 export * from './flow'
 export * from './verse'
 export * from './signal'
 export * from './store'
 export * from './task'
+
+export default Orap
