d804e5c4548e857eb11dbf70aebb87f0bf0610b3
MuRong <hi@mrong.me>
2024-10-08 18:59:38 +0800

    chore: modify debug namespace

--- packages/utils/src/common/debug.ts
+++ packages/utils/src/common/debug.ts
@@ -1,5 +1,10 @@
+/* eslint-disable no-console */
 import debugFn from 'debug'
 
 export function debug(workspace: string) {
-  return debugFn(`ora-stack:${workspace}`)
+  const d = debugFn(`DEBUG:ora-stack:${workspace}`)
+  d.log = (...args: any[]) => {
+    console.log(...args)
+  }
+  return d
 }
